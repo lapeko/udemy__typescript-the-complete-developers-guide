650401874d8d8e45c1613541141b150bf462ce5c
lapeko <lapeko88@gmail.com>
2023-08-31 10:48:51 +0200

    add google maps

--- chapter9/src/script.ts
+++ chapter9/src/script.ts
@@ -3,3 +3,11 @@ import { User } from "./user";
 
 console.log(new User());
 console.log(new Company());
+
+new google.maps.Map(document.getElementById("map") as HTMLDivElement, {
+  zoom: 1,
+  center: {
+    lat: 1,
+    lng: 1,
+  },
+});
