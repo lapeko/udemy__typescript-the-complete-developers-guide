08ee1a6a500ede970e9d372b0969fc7a07de75b4
lapeko <lapeko88@gmail.com>
2023-09-17 20:17:06 +0200

    add auth middleware

--- chapter14/src/router.ts
+++ chapter14/src/router.ts
@@ -4,11 +4,12 @@ import { homeRouter } from "./routes/home";
 import { loginRouter } from "./routes/login";
 import { protectedRouter } from "./routes/protected";
 import { logoutRouter } from "./routes/logout";
+import { authGuard } from "./middlewares/authGuard";
 
 export const router = Router();
 
 router.use("/", homeRouter);
 router.use("/login", loginRouter);
 router.use('/logout', logoutRouter)
-router.use("/protected", protectedRouter);
+router.use("/protected", authGuard, protectedRouter);
 
--- chapter14/src/routes/protected.ts
+++ chapter14/src/routes/protected.ts
@@ -2,16 +2,8 @@ import { Router } from "express";
 
 export const protectedRouter = Router();
 
-protectedRouter.get("/", (req, res) => {
-    if (req.session?.loggedIn) return res.send(`
-        <div>
-            <p>Welocme to protected route</p>
-        </div>
-    `);
-    res.status(403).send(`
-        <div>
-            <p>Denied</p>
-            <a href="/login">Try to login</a>
-        </div>
-    `)
-});
+protectedRouter.get("/", (req, res) => res.send(`
+  <div>
+    <p>Welocme to protected route</p>
+  </div>
+`));
