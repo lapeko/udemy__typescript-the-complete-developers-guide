94844257a1d83cf1318a45c5810cc76af7c6e67f
lapeko <lapeko88@gmail.com>
2023-09-18 20:43:56 +0200

    complete chapter 15 - decorators

--- chapter15/src/script.ts
+++ chapter15/src/script.ts
@@ -1,15 +1,37 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = "red";
 
   get formattedColor(): string {
     return `This boats color is ${this.color}`;
   }
 
-  @errorLogger("Oops. An error happened =(")
-  pilot(): void {
+  // @errorLogger("Oops. An error happened =(")
+  pilot(
+    @parameterDecorator arg: number,
+    @parameterDecorator arg2: number
+  ): void {
     throw new Error("Error !");
     console.log("swish");
   }
+}
+
+function classDecorator(constructor: typeof Boat) {
+  console.log("Class decorator");
+  console.log("Constructor:", constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log("ParameterDecorator");
+  console.log("Key", key);
+  console.log("Index", index);
+}
+
+function testDecorator(target: any, key: string) {
+  console.log("TestDecorator");
+  console.log("Target", target[key]);
+  console.log("Key", key);
 }
 
 function errorLogger(message: string) {
@@ -25,4 +47,4 @@ function errorLogger(message: string) {
   };
 }
 
-new Boat().pilot();
+new Boat();
