a34417a306538634d2d5a499b96915837dd2a958
lapeko <lapeko88@gmail.com>
2023-09-30 18:11:59 +0200

    improve controller decorator

--- chapter16/src/controllers/decorators/controller.ts
+++ chapter16/src/controllers/decorators/controller.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 
 import { appRouter } from '../../../app-router';
 import { MetadataKey, Method } from '../../model';
-import { RequestHandler } from "express";
+import {Request, RequestHandler, Response} from "express";
 
 export const controller = (prefix: string) => (constructor: Function) => {
   Object.getOwnPropertyNames(constructor.prototype)
@@ -15,7 +15,6 @@ export const controller = (prefix: string) => (constructor: Function) => {
     .forEach(({ key, path }) => {
       const method = Reflect.getMetadata(MetadataKey.Method, constructor.prototype, key);
       const middlewares = Reflect.getMetadata(MetadataKey.Middlewares, constructor.prototype, key) as RequestHandler[] | void || [];
-      middlewares.forEach(middleware => appRouter.use(path, middleware));
-      appRouter[method as Method](path, constructor.prototype[key]);
+      appRouter[method as Method](path, middlewares, constructor.prototype[key]);
     });
 };
